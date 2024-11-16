ee81acddd0b8e3a444d0e6547bff20d3d2cee5e6
maihiang <oumaekumiko160@outlook.com>
2024-11-16 14:09:11 +0800

    make 0-4 better

--- User/demo.c
+++ User/demo.c
@@ -16,11 +16,11 @@ mode可取0-5
 int mode = 0;
 
 /**
- * @brief       演示立方体3D旋转
+ * @brief       演示各个界面
  * @param       无
  * @retval      无
  */
-static void demo_show_cube(void)
+static void demo_show_ui(void)
 {
     uint16_t x_scan;
     uint16_t y_scan;
@@ -60,8 +60,23 @@ static void demo_show_cube(void)
             if ((x_scan >= 10)&&(x_scan <= 40)&&(y_scan >= 10)&&(y_scan <= 30))
             {
                 mode = 0;
-                atk_md0280_clear(ATK_MD0280_BLACK);
+                atk_md0280_clear(ATK_MD0280_WHITE);
             }
+
+            if ((x_scan >= 1)&&(x_scan <= 32)&&(y_scan >= 152)&&(y_scan <= 167))
+            {
+                mode--;
+                mode=mode?mode:4;  
+                atk_md0280_clear(ATK_MD0280_WHITE);
+            }
+            
+            if ((x_scan >= 207)&&(x_scan <= 238)&&(y_scan >= 152)&&(y_scan <= 167))
+            {
+                mode=(mode+1)%4;
+                mode=mode?mode:4;
+                atk_md0280_clear(ATK_MD0280_WHITE);
+            }
+
         }
         
     }
@@ -69,34 +84,89 @@ static void demo_show_cube(void)
     switch(mode)
     {
         case 0:
-            atk_md0280_fill(0,0,109,149,ATK_MD0280_WHITE);
-            atk_md0280_fill(130,0,239,149,ATK_MD0280_WHITE);
-            atk_md0280_fill(0,170,109,319,ATK_MD0280_WHITE);
-            atk_md0280_fill(130,170,239,319,ATK_MD0280_WHITE);
+            atk_md0280_fill(0,150,239,169,ATK_MD0280_BLACK);
+            atk_md0280_fill(110,0,129,319,ATK_MD0280_BLACK);
+
+            atk_md0280_show_string(40, 74, ATK_MD0280_LCD_WIDTH, 12, "Show 1", ATK_MD0280_LCD_FONT_12, ATK_MD0280_BLACK);
+            atk_md0280_show_string(160, 74, ATK_MD0280_LCD_WIDTH, 12, "Show 2", ATK_MD0280_LCD_FONT_12, ATK_MD0280_BLACK);
+            atk_md0280_show_string(40, 234, ATK_MD0280_LCD_WIDTH, 12, "Show 3", ATK_MD0280_LCD_FONT_12, ATK_MD0280_BLACK);
+            atk_md0280_show_string(160, 234, ATK_MD0280_LCD_WIDTH, 12, "Show 4", ATK_MD0280_LCD_FONT_12, ATK_MD0280_BLACK);
             break;
         
         case 1:
             atk_md0280_show_string(120, 160, ATK_MD0280_LCD_WIDTH, 32, "1", ATK_MD0280_LCD_FONT_32, ATK_MD0280_RED);
             atk_md0280_fill(10,10,40,30,ATK_MD0280_BLACK);
             atk_md0280_show_string(12, 15, ATK_MD0280_LCD_WIDTH, 12, "BACK", ATK_MD0280_LCD_FONT_12, ATK_MD0280_WHITE);
+
+            atk_md0280_draw_line(1,152,1,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(1,167,32,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(32,167,32,152,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(32,152,1,152,ATK_MD0280_BLACK);
+            atk_md0280_show_string(5, 149, ATK_MD0280_LCD_WIDTH, 16, "pre", ATK_MD0280_LCD_FONT_16, ATK_MD0280_BLACK);
+            
+            atk_md0280_draw_line(207,152,207,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(207,167,238,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(238,167,238,152,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(238,152,207,152,ATK_MD0280_BLACK);
+            atk_md0280_show_string(211, 149, ATK_MD0280_LCD_WIDTH, 16, "nex", ATK_MD0280_LCD_FONT_16, ATK_MD0280_BLACK);
             break;
 
         case 2:
             atk_md0280_show_string(120, 160, ATK_MD0280_LCD_WIDTH, 32, "2", ATK_MD0280_LCD_FONT_32, ATK_MD0280_RED);
             atk_md0280_fill(10,10,40,30,ATK_MD0280_BLACK);
             atk_md0280_show_string(12, 15, ATK_MD0280_LCD_WIDTH, 12, "BACK", ATK_MD0280_LCD_FONT_12, ATK_MD0280_WHITE);
+
+            
+            atk_md0280_draw_line(1,152,1,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(1,167,32,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(32,167,32,152,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(32,152,1,152,ATK_MD0280_BLACK);
+            atk_md0280_show_string(5, 149, ATK_MD0280_LCD_WIDTH, 16, "pre", ATK_MD0280_LCD_FONT_16, ATK_MD0280_BLACK);
+
+
+            atk_md0280_draw_line(207,152,207,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(207,167,238,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(238,167,238,152,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(238,152,207,152,ATK_MD0280_BLACK);
+            atk_md0280_show_string(211, 149, ATK_MD0280_LCD_WIDTH, 16, "nex", ATK_MD0280_LCD_FONT_16, ATK_MD0280_BLACK);
             break;
             
         case 3:
             atk_md0280_show_string(120, 160, ATK_MD0280_LCD_WIDTH, 32, "3", ATK_MD0280_LCD_FONT_32, ATK_MD0280_RED);
             atk_md0280_fill(10,10,40,30,ATK_MD0280_BLACK);
             atk_md0280_show_string(12, 15, ATK_MD0280_LCD_WIDTH, 12, "BACK", ATK_MD0280_LCD_FONT_12, ATK_MD0280_WHITE);
+            
+            atk_md0280_draw_line(1,152,1,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(1,167,32,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(32,167,32,152,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(32,152,1,152,ATK_MD0280_BLACK);
+            atk_md0280_show_string(5, 149, ATK_MD0280_LCD_WIDTH, 16, "pre", ATK_MD0280_LCD_FONT_16, ATK_MD0280_BLACK);
+
+
+            atk_md0280_draw_line(207,152,207,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(207,167,238,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(238,167,238,152,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(238,152,207,152,ATK_MD0280_BLACK);
+            atk_md0280_show_string(211, 149, ATK_MD0280_LCD_WIDTH, 16, "nex", ATK_MD0280_LCD_FONT_16, ATK_MD0280_BLACK);
             break;
 
         case 4:
             atk_md0280_show_string(120, 160, ATK_MD0280_LCD_WIDTH, 32, "4", ATK_MD0280_LCD_FONT_32, ATK_MD0280_RED);
             atk_md0280_fill(10,10,40,30,ATK_MD0280_BLACK);
             atk_md0280_show_string(12, 15, ATK_MD0280_LCD_WIDTH, 12, "BACK", ATK_MD0280_LCD_FONT_12, ATK_MD0280_WHITE);
+            
+            atk_md0280_draw_line(1,152,1,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(1,167,32,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(32,167,32,152,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(32,152,1,152,ATK_MD0280_BLACK);
+            atk_md0280_show_string(5, 149, ATK_MD0280_LCD_WIDTH, 16, "pre", ATK_MD0280_LCD_FONT_16, ATK_MD0280_BLACK);
+
+
+            atk_md0280_draw_line(207,152,207,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(207,167,238,167,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(238,167,238,152,ATK_MD0280_BLACK);
+            atk_md0280_draw_line(238,152,207,152,ATK_MD0280_BLACK);
+            atk_md0280_show_string(211, 149, ATK_MD0280_LCD_WIDTH, 16, "nex", ATK_MD0280_LCD_FONT_16, ATK_MD0280_BLACK);
             break;
     }
 
@@ -126,11 +196,11 @@ void demo_run(void)
     }
     
     /* ATK-MD0280模块LCD清屏 */
-    atk_md0280_clear(ATK_MD0280_BLACK);
+    atk_md0280_clear(ATK_MD0280_WHITE);
     
     while (1)
     {
         /* 演示立方体3D旋转 */
-        demo_show_cube();
+        demo_show_ui();
     }
 }
